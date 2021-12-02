3e23ee59a483743cdacbf1f2d38cc3d2cd10e548
tolga inkaya <redlof@tolgas-MacBook-Pro.local>
2021-12-02 03:03:51 +0300

    practicality_beats_purityy

---
+++ practicality_beats_purity.py
@@ -0,0 +1,9 @@
+"""
+If the possible benefits (e.g., better performance) are larger than the possible negative
+effects (e.g., affected maintainability), the real-world coding problems may find an excuse 
+for making an exception to the rules. Practicality then becomes more important than purity.
+
+If you need to write an 85-character long line of code because splitting it into two 
+separate lines affects readability, do it. If you need to keep compatibility with previously
+written code and use CamelCase instead of snake_case, do it. Rules sometimes have to be broken, exceptions have to be made.
+"""
